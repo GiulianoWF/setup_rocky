864fb331d493bf8406e549908cd0a7f99dc78de3
GiulianoWF <Giuliano.Floriani@gmail.com>
2024-07-29 06:39:54 -0400

    Flags to make fusion run

--- setup_qt.sh
+++ setup_qt.sh
@@ -12,3 +12,14 @@ dnf -y install qt5-qtwebengine-devtools
 dnf -y install qt5-qtwebengine-devel
 dnf -y install qt5-qtwebkit-devel
 
+# Setup no sandbox to enable qt to run
+
+# Check and add QTWEBENGINE_CHROMIUM_FLAGS to sudoers if not already present
+if ! grep -q "Defaults env_keep += \"QTWEBENGINE_CHROMIUM_FLAGS\"" /etc/sudoers; then
+    echo 'Defaults env_keep += "QTWEBENGINE_CHROMIUM_FLAGS"' | EDITOR='tee -a' visudo
+fi
+
+# Check and create qt_flags.sh if it doesn't exist
+if [ ! -f "/etc/profile.d/qt_flags.sh" ]; then
+    echo 'export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox"' > /etc/profile.d/qt_flags.sh
+fi
